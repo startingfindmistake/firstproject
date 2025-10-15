1a0f89121fb21f31c690ba4512509fc8f33b6d19
Startingfindmistakesamsung <dudgh2328@gmail.com>
2025-10-15 17:27:10 +0900

    id를 조회해 데이터 가져오기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,14 +1,21 @@
 package com.example.firstproject.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article; //Article 클래스 임포트 확인
 import com.example.firstproject.repository.ArticleRepository;
 
+import ch.qos.logback.core.model.Model;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j      // 데이터를 확인하는 로깅 기능을 위한 어노테이션 추가
 @Controller // 컨트롤러 선언
 
 public class ArticleController {
@@ -21,14 +28,37 @@ public class ArticleController {
 
     @PostMapping("/articles/create")    //URL 요청 접수
     public String createArticle(ArticleForm form) {     // 메서드 생성 및 반환값 작성, 폼 데이터로 DTO로 받기
-        System.out.println(form.toString());        //DTO에 폼 데이터가 잘 담겼는지 확인
+
+        log.info(form.toString()); // 아래 주석한줄 내용을 대체하는 로깅 코드 추가
+        //System.out.println(form.toString());        //DTO에 폼 데이터가 잘 담겼는지 확인
+
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());     // DTO가 엔티티로 잘 변환되는지 확인 출력
+        log.info(article.toString());   // 아래 주석한줄 내용을 대체하는 로깅 코드 추가
+        //System.out.println(article.toString());     // DTO가 엔티티로 잘 변환되는지 확인 출력
+
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);       // article 엔티티를 저장해 saved 객체에 반환
-        System.out.println(saved.toString());       // article이 DB에 잘 저장되는지 확인 출력
+        log.info(saved.toString());     // 아래 주석한줄 내용을 대체하는 로깅 코드 추가
+        //System.out.println(saved.toString());       // article이 DB에 잘 저장되는지 확인 출력
         return "";
     }
+
+
     
+    @GetMapping("/articles/{id}") // 데이터 조회 요청 접수
+    public String show(@PathVariable Long id) {     // 매개변수로 id 받아 오기
+        log.info("id= " + id);  // id를 잘 받았는지 확인하는 로그 찍기
+
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // .orElse 설명: id 값으로 데이터를 찾을 때 해당 id 값이 없으면 null을 반환해라
+        // 2. 모델에 데이터 등록하기
+        // 3. 뷰 페이지 반환하기
+
+
+        return "";
+
+    //@PathVariable은 URL 요청으로 들어온 전달값을 컨트롤러의 매개변수로 가져오는 어노테이션
+    }
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor     // Article() 생성자를 대체하는 어노테이션 추가
+@ToString       // toString()메서드를 대체하는 어노테이션 추가
 @Entity //엔티티 선언 (이 어노테이션이 붙은 크래스를 기반으로 DB에 테이블이 생성 됩니다. 테이블 이름은 클래스 이름과 동일하게 Article로 생성됩니다.)
 public class Article {
     @Id     //엔티티 대표값 지정
@@ -15,6 +19,9 @@ public class Article {
     @Column     // content 필드 선언, DB 테이블의 content 열과 연결됨
     private String content;
 
+
+
+    /* 롬복으로 엔티티 리팩터링하기
     //Article 생성자 추가
     public Article(Long id, String title, String content) {
         this.id = id;
@@ -27,6 +34,8 @@ public class Article {
     public String toString() {
         return "Article [id=" + id + ", title=" + title + ", content=" + content + "]";
     }
+
+    */
 
     
     
